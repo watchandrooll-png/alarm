b4fd46a307744f9f16a6fd4c33de8cd5ec6c083b
watchandrooll-png <watchandrooll@gmail.com>
2025-10-16 17:18:19 -0400

    alarrrrm

---
+++ alarm.py
@@ -0,0 +1,21 @@
+import datetime
+from playsound import playsound
+import time
+import math
+
+while True:
+    alarmer = input("In how many hours do you want the alarm to go off at?\nChoice: ")
+    tim = datetime.datetime.now()
+    minute = tim.minute
+    hour = float(tim.hour) + (minute / 100) 
+    timetoalert = float(alarmer) + hour
+    while True:
+        tim = datetime.datetime.now()
+        minute = tim.minute
+        hour = float(tim.hour) + (minute / 100)
+        print(hour)
+        print(timetoalert)
+        if timetoalert <= hour:
+            playsound("y\preview.mp3")
+            break
+        time.sleep(1)
